c6cfd3a89e01f2a30a98d1d8fb830bee6d2df414
MCTC <rashelmahmudraj1998@gmail.com>
2023-06-09 14:53:48 +0600

    added semester

--- src/app/modules/academicSemester/academicSemesterModel.ts
+++ src/app/modules/academicSemester/academicSemesterModel.ts
@@ -1,4 +1,6 @@
+import status from 'http-status';
 import { Schema, model } from 'mongoose';
+import ApiError from '../../../errors/ApiError';
 import {
   academicSemesterCodes,
   academicSemesterTitles,
@@ -40,6 +42,18 @@ const academicSemesterSchema = new Schema<IAcademiSemester>(
     timestamps: true,
   }
 );
+
+academicSemesterSchema.pre('save', async function (next) {
+  const isExist = await AcademicSemester.findOne({
+    title: this.title,
+    year: this.year,
+  });
+  if (isExist) {
+    throw new ApiError(status.CONFLICT, 'academic sermister already exsist..');
+  }
+  next();
+});
+
 export const AcademicSemester = model<IAcademiSemester, AcademicSemesterModel>(
   'AcademicSemester',
   academicSemesterSchema
